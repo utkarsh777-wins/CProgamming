1a552da9d6b341934c4e975a608da0f7b9d1a7b2
utkarsh777-wins <utkarsh777wins@gmail.com>
2026-02-27 03:24:12 +0530

    let us see CA1 tomo

--- iPlay/playgrounds.c
+++ iPlay/playgrounds.c
@@ -4,9 +4,12 @@ int main() {
     // int b = 5 + 7 * 4 - 9  * (3,2);
     // printf("%lf", b);
     // printf("%d", b);
-    int a = -1; int b = 4; int c = 1; int d;
-    d = ++a && ++b || ++c;
-    printf("%d, %d, %d, %d\n", a, b, c, d);
+    // int a = -1; int b = 4; int c = 1; int d;
+    // d = ++a && ++b || ++c;
+    // printf("%d, %d, %d, %d\n", a, b, c, d);
+    // printf("%d", 012 + 10);
+    printf("%d", 5 + 3 * 2 % 4);
+    printf("%d", 6/4);
     
     return 0;
 }
