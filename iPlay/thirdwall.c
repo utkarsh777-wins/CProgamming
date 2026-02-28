15af4f5aada048cd8741bb6a9968b1916a225596
utkarsh777-wins <utkarsh777wins@gmail.com>
2026-03-01 01:02:58 +0530

    extra dimension

---
+++ iPlay/thirdwall.c
@@ -0,0 +1,14 @@
+// faboonacci sequence
+#include <stdio.h>
+
+int fabo(int n);
+
+int main() {
+    int n;
+    printf("enter you number till sequence: ");
+    scanf("%d", &n);
+}
+
+int fabo(int n) {
+
+}
