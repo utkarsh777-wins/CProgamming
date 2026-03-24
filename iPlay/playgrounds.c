a4b0b7ec94f5a5a63c8c3ab47cfeec5a2d8d66e3
utkarsh777-wins <utkarsh777wins@gmail.com>
2026-03-24 17:29:19 +0530

    shi

--- iPlay/playgrounds.c
+++ iPlay/playgrounds.c
@@ -14,38 +14,74 @@
     
 //     return 0;
 // }
-#include <stdio.h> 
+// #include <stdio.h> 
 
-void getDetails();
-void yes();
-void no();
+// void getDetails();
+// void yes();
+// void no();
 
-int main() { 
-    // int i; 
-    // for (i = 0; i < 5; i++) { 
-    //     if (i == 2) 
-    //     goto end; 
-    // } 
-    // end: 
-    // return i;
-    // int tem = 6;
-    // printf("%d", -tem);
-    getDetails();
-    // printf("%d", 3&&990);
+// int main() { 
+//     // int i; 
+//     // for (i = 0; i < 5; i++) { 
+//     //     if (i == 2) 
+//     //     goto end; 
+//     // } 
+//     // end: 
+//     // return i;
+//     // int tem = 6;
+//     // printf("%d", -tem);
+//     getDetails();
+//     // printf("%d", 3&&990);
+//     return 0;
+// }
+
+// void getDetails() {
+//     char get;
+//     printf("Enter: (Y/N): ");
+//     scanf("%c", &get);
+//     if(get == 'Y' || get == 'y') {yes();} else if(get == 'N' || get == 'n') {no();} else{ printf("Invalid");}
+// }
+
+// void yes() {
+//     printf("yessssssssssssss!");
+// }
+
+// void no() {
+//     printf("noooooooooo!");
+// }
+#include <stdio.h>
+
+int takeInput(int arr[], int n) {
+    for(int i=1; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+}
+
+void printArray(int arr[], int n) {
+    for(int i=1; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
+// int main() {
+//     int a[9];
+//     takeInput(a, 9);
+//     printArray(a, 9);
+    
+//     return 0;
+// }
+int main() {
+    int n;
+    printf("number of elements inside the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for(int i=0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
+
     return 0;
-}
-
-void getDetails() {
-    char get;
-    printf("Enter: (Y/N): ");
-    scanf("%c", &get);
-    if(get == 'Y' || get == 'y') {yes();} else if(get == 'N' || get == 'n') {no();} else{ printf("Invalid");}
-}
-
-void yes() {
-    printf("yessssssssssssss!");
-}
-
-void no() {
-    printf("noooooooooo!");
 }
