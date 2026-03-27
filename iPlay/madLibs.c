0e015d27b99f1fde169bcbdfe33daf97570a4ac6
utkarsh777-wins <utkarsh777wins@gmail.com>
2026-03-28 00:14:38 +0530

    madLibs game

---
+++ iPlay/madLibs.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char noun[50] = "";
+    char verb[50] = "";
+    char adjective1[50] = "";
+    char adjective2[50] = "";
+    char adjective3[50] = "";
+
+    //first adjective 
+    printf("enter an adjective (description): ");
+    fgets(adjective1, sizeof(adjective1), stdin);
+    adjective1[strlen(adjective1) - 1] = '\0';
+    
+    //first noun
+    printf("enter a noun (animal or person): ");
+    fgets(noun, sizeof(noun), stdin);
+    noun[strlen(noun) - 1] = '\0';
+    
+    //second adjective
+    printf("enter an adjective (description): ");
+    fgets(adjective2, sizeof(adjective2), stdin);
+    adjective2[strlen(adjective2) - 1] = '\0';
+    
+    //verb
+    printf("enter a verb (action in v3): ");
+    fgets(verb, sizeof(verb), stdin);
+    verb[strlen(verb) - 1]  = '\0';
+    
+    //third adjective 
+    printf("enter an adjective (description): ");
+    fgets(adjective3, sizeof(adjective3), stdin);
+    adjective3[strlen(adjective3) - 1] = '\0';
+
+    //story time
+    printf("%s\n", adjective1);
+    printf("%s\n", noun);
+    printf("%s\n", adjective2);
+    printf("%s\n", verb);
+    printf("%s\n", adjective3);
+}
