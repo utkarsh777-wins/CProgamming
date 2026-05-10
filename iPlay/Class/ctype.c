345974df2e80aa639ad585dc0eb5ef95718ce7fc
utkarsh777-wins <utkarsh777wins@gmail.com>
2026-05-10 13:26:12 +0530

    finally no more classes

--- iPlay/Class/ctype.c
+++ iPlay/Class/ctype.c
@@ -1,64 +1,94 @@
-// #include <stdio.h>
-// #include <string.h>
-// #include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-// // no. of occurences of a character in a string
-// int main() {
+// no. of occurences of a character in a string
+int main() {
 
-//     char str[100];
-//     int i=0, c;
-//     int visited[100] = {0};
+    char str[100];
+    int i=0, c;
+    int visited[100] = {0};
 
-//     printf("Enter your string: ");
-//     fgets(str, sizeof(str), stdin);
+    printf("Enter your string: ");
+    fgets(str, sizeof(str), stdin);
 
-//     str[strcspn(str, "\n")] = '\0';    //removing newline 
+    str[strcspn(str, "\n")] = '\0';    //removing newline 
 
-//     while(str[i]!='\0') {
+    while(str[i]!='\0') {
 
-//         if(visited[i] == 1) {continue;} 
-//         c=1;
-//         for(int j=i+1; str[j]!='\0'; j++) {
-//             if(str[i] == str[j]) {
-//                 c++;
-//                 visited[j] = 1;
-//             }
-//         } printf(" %c occur %d times\n", str[i], c);
+        if(visited[i] == 1) {
+            i++;
+            continue;
+        } 
 
-//         i++;
+        c=1;
+        
+        for(int j=i+1; str[j]!='\0'; j++) {
+            if(str[i] == str[j]) {
+                c++;
+                visited[j] = 1;
+            }
+        } printf(" %c occur %d times\n", str[i], c);
 
-//     }
+        i++;
 
-//     printf("Ain't that neat^^\n");
+    }
 
-//     return 0;
+    printf("Ain't that neat^^\n");
 
-// }
+    return 0;
+
+}
 
 
 
 // count the number of occurences of uppercase in a string 
 
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <ctype.h>
 
-int main() {
-    //toupper cvonverts a single character into upper case
+// char countUpper(char str[]) {
 
-    char str[100];
-    printf("Enter: ");
-    fgets(str, sizeof(str), stdin);
+//     //using isuper function 
+//     int count =0;
+//     for(int i=0; str[i]!='\0'; i++) {
+        
+//         if(isupper(str[i])) {
+//             count+=1;
+//         }
 
+//     }
 
-    int count=0;
-    for(int i=0; str[i]!='\0'; i++) {
-        if(str[i] == toupper(str[i])) {
-            count ++;
-        }
-    }
+//     return count;
+// }
 
-    printf("%d", count);
+// int main() {
+//     //toupper cvonverts a single character into upper case
 
-    return 0;
-}
+//     char str[100];
+//     printf("Enter: ");
+//     fgets(str, sizeof(str), stdin);
+
+//     //incorrect, this will count anything that isn't an uppercase 
+//     // int count=0;
+//     // for(int i=0; str[i]!='\0'; i++) {
+//     //     if(str[i] == toupper(str[i])) {
+//     //         count ++;
+//     //     }
+//     // }
+    
+//     // the manual way 
+//     // int count = 0;
+//     // for(int i=0; str[i]!='\0'; i++) {
+//     //     if(str[i] >= 'A' && str[i] <= 'Z') {
+//     //         count+=1;
+//     //     }
+//     // }
+
+//     int count = countUpper(str);
+
+//     printf("%d", count);
+
+//     return 0;
+// }
--- iPlay/Class/struct.c
+++ iPlay/Class/struct.c
@@ -1,11 +1,88 @@
+// #include <stdio.h>
+
+// struct cars {
+//     char *name;
+//     int horsePower;
+//     float fuelCapacity;
+// };
+
+// int main() {
+
+// }
+
 #include <stdio.h>
 
-struct cars {
+
+// struct Student {
+//     char name[20];
+//     int reg;
+//     float cgpa;
+// };
+
+// //manual input 
+// int main() {
+//     // struct Student s1, s2
+//     struct Student s1 = {"rithik", 12500, 7.5};
+//     printf("Name= %s\n", s1.name);
+//     printf("Reg. No. = %d\n", s1.reg);
+//     printf("CGPA= %.2f\n", s1.cgpa);
+
+// }
+
+//user input 
+// struct Student {
+//     char name[20];
+//     int reg;
+//     float cgpa;
+// }s[3];
+
+// //user input 
+// // int main() {
+// //     printf("Enter name: ");
+// //     scanf("%s", s1.name);
+// //     printf("Enter reg: ");
+// //     scanf("%d", &s1.reg);
+// //     printf("Enter cgpa: ");
+// //     scanf("%f", &s1.cgpa);
+
+// //     printf("%s\n", s1.name);
+// //     printf("%d\n", s1.reg);
+// //     printf("%.2f\n", s1.cgpa);
+
+// // }
+
+// //struct array
+// int main() {
+//     for(int i=0; i<3; i++) {
+//         printf("Details of %d student: \n", i+1);
+//         printf("Name: ");
+//         scanf("%s", s[i].name);
+//         printf("Reg no: ");
+//         scanf("%d", &s[i].reg);
+//         printf("CGPA: ");
+//         scanf("%f", &s[i].cgpa);
+        
+//     }
+
+//     for(int i=0; i<3; i++) {
+
+//         printf("Name = %s | Reg no: %d | CGPA: %.2f\n", s[i].name, s[i].reg, s[i].cgpa);
+
+//     }
+// }
+
+struct Book{
+
     char *name;
-    int horsePower;
-    float fuelCapacity;
+    int total_pages;
+    float price;
+
 };
 
 int main() {
+    struct Book pr= {"C", 120, 255.45};
+    struct Books *python;
+    python = &pr;;
 
+    printf("Name= %s", python->name);
 }
