a3a5da112235c8e39fbffdfa59bf584c30f44527
utkarsh777-wins <utkarsh777wins@gmail.com>
2026-06-14 13:09:27 +0530

    cpp basics

--- CPPintro/9pattern.cpp
+++ CPPintro/9pattern.cpp
@@ -1,18 +1,32 @@
 /*
-star triangle 
+    *
+   ***
+  *****
+ *******
+*********
+// star triangle 
 */
 
-#include <iostream>
-int main() {
-    for(int i=1; i<=5; i++) {
-        for(int j=0; j<5-i; j++) {
-            std::cout << " ";
-        }
-        for(int k=0; k<i; k++) {
-            std::cout << "*";
-        }
-        std::cout << std::endl;
-    }
+// #include <iostream>
+// int main() {
+//     for(int i=1; i<=5; i++) {
+//         for(int j=0; j<5-i; j++) {
+//             std::cout << " ";
+//         }
+//         for(int k=0; k<i; k++) {
+//             std::cout << "*";
+//         }
+//         for(int m=0; m<i-1; m++) {
+//             std::cout << "*";
+//         }
+//         std::cout << std::endl;
+//     }
 
-    return 0;
+//     return 0;
+// }
+
+#include <iostream>
+
+int main() {
+    
 }
