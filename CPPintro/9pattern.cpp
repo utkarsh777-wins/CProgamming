901a600c27fe828cb3a0087f053458f3142ae91f
utkarsh777-wins <utkarsh777wins@gmail.com>
2026-06-15 23:54:25 +0530

    cpp basics

--- CPPintro/9pattern.cpp
+++ CPPintro/9pattern.cpp
@@ -26,7 +26,191 @@
 // }
 
 #include <iostream>
+/*
+****
+****
+****
+****
+*/
+void sqStar() {
+
+    for(int i=1; i<5; i++) {
+        for(int j=1; j<5; j++) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+}
+
+
+/*
+*
+**
+***
+****
+*****
+*/
+void rightStar(int n) {
+    int i=0;
+    while(i<n) {
+        int j=0;
+        do {
+            std::cout << "*";
+            j++;
+        } while(j<=i);
+        
+        std::cout << std::endl;
+        i++;
+    }
+}
+
+/*
+1
+1 2
+1 2 3
+1 2 3 4 
+1 2 3 4 5
+*/
+
+void RightCount(int n) {
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            std::cout << i << " ";
+
+        } printf("\n");
+    }
+}
+/*
+1
+2 2
+3 3 3
+4 4 4 4 
+5 5 5 5 5
+*/
+void rightCount(int n) {
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            std::cout << j << " ";
+
+        } printf("\n");
+    }
+}
+
+void rightInvStar(int n) {
+    int i=n;
+    while(i>0) {
+        int j=0;
+        do {
+            std::cout << "*" << " ";
+            j++;
+        } while(j<i);
+        std::cout << std::endl;
+        i--;
+    }
+    std::cout << std::endl;
+}
+
+void InvRightCount(int n) {
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n-i+1; j++) {
+            std::cout << j << " ";
+        }
+        std::cout << std ::endl;
+    } printf("\n");
+}
+
+/*
+    *
+   ***
+ *******
+*********
+*/
+
+void printStar(int n) {
+    for(int i=0; i<n; i++) {
+        
+        //spaces
+        for(int j=0; j<(n-i-1); j++) {
+            std::cout << " ";
+        }
+        
+        //stars
+        for(int k=0; k<(2*i+1); k++) {
+            std::cout << "*";
+        }
+        
+        //spaces
+        for(int j=0; j<(n-i-1); j++) {
+            std::cout << " ";
+        }
+        
+        printf("\n");
+    }
+
+}
+
+    /*
+    *********
+     *******
+       ***
+        *
+    */
+
+void printRstar(int n) {
+    for(int i=0; i<n; i++) {
+        //spaces 
+        for(int j=0; j<i; j++) {
+            std::cout << " ";
+        }
+
+        //stars 
+        for(int s=0; s<(n*2 - i*2 - 1); s++) {
+            std::cout << "*";
+        }
+
+        //spaces
+        for(int k=0; k<i; k++) {
+            std::cout << " ";
+        }
+
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void printSymmetric(int n) {
+    for(int i=1; i<n*2; i++) {
+        if(i<=n) {
+            for(int j=0; j<i; j++) {
+                std::cout << "*";
+            }
+        } else if(i > n && i <= n*2) {
+            for(int j=0; j<n*2-i; j++) {
+                std::cout << "*";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main() {
-    
+    sqStar();
+    std::cout << std::endl;
+
+    rightStar(5);
+    printf("\n");
+    rightCount(5);
+    printf("\n");
+    int n;
+    std::cin >> n;
+    RightCount(n);
+    rightInvStar(n);
+    InvRightCount(n);
+    printStar(n);
+    printRstar(n);
+    printSymmetric(n);
+
+
+    return 0;
 }
