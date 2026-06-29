94b9cd2b06ef896a988a26ce46af2d7abc0e731f
utkarsh777-wins <utkarsh777wins@gmail.com>
2026-06-29 21:12:36 +0530

    number guessing game

--- iPlay/MICROprojects/guessNum.c
+++ iPlay/MICROprojects/guessNum.c
@@ -2,33 +2,82 @@
 #include <stdlib.h>
 #include <time.h>
 
+// int main() {
+
+//     srand(time(NULL));
+
+//     int rnd=0, user;
+
+//     //making it so the range of the random number is in b/w 1-100
+//     rnd = (rand() % 100) + 1;
+
+//     while(1) {
+
+//         // user input
+//         printf("Guess the the random number (1-100):) ");
+//         if(scanf("%d", &user) != 1) {return 0;}
+
+//         if(user == rnd) {
+
+//             printf("Correct!! You nailed it!");
+//             break;
+        
+//         }
+
+//         else if(user < rnd) {printf("Go higher!\n");}
+//         else if(user > rnd) {printf("Go lower!\n");}
+
+//     }
+
+//     return 0;
+// }
+// //enhance the current interface
+
 int main() {
 
-    srand(time(NULL));
+    int guess = 0;
+    int lives = 10;
+    int min = 1;
+    int max = 100;
 
-    int rnd=0, user;
+    // using 1 as offset for cases when % outputs 0;
+    int answer = (rand() % (max - min + 1)) + min;
 
-    //making it so the range of the random number is in b/w 1-100
-    rnd = (rand() % 100) + 1;
+    printf("*** NUMBER GUESSING GAME ***\n");
 
-    while(1) {
+    do {
 
-        // user input
-        printf("Guess the the random number (1-100):) ");
-        if(scanf("%d", &user) != 1) {return 0;}
+        //if all lives are burnt 
+        if(lives == 0) 
+        {
 
-        if(user == rnd) {
+            printf("\n");
+            printf("Game Over :(\n");
+            return 0;
 
-            printf("Correct!! You nailed it!");
-            break;
-        
+
         }
 
-        else if(user < rnd) {printf("Go higher!\n");}
-        else if(user > rnd) {printf("Go lower!\n");}
+        
+        printf("\n");
+        printf("Guess a number between %d - %d\n", min, max);
+        scanf("%d", &guess);
+        
+        // hints
+        if(guess < answer) {printf("TOO LOW\n");}
+        else if(guess > answer) {printf("TOO HIGH\n");}
+        else {printf("CORRECT\n");}
 
-    }
 
+        printf("<3 Remaining lives: %d\n", lives);
+        
+        // one life burns on each failed guess
+        lives--;
+        
+    } while(guess != answer);
+    
+    printf("The answer is: %d\n", answer);
+    printf("You saved %d lives\n", lives);
+    
     return 0;
 }
-//enhance the current interface 
